d0824bb4baeff443a31a1b9dbdc0d65e7be68576
cesnakas <10352420+cesnakas@users.noreply.github.com>
2020-08-27 01:28:52 +0400

    1.2.11

---
+++ local/templates/main/js/common.js
@@ -0,0 +1,16 @@
+// navigation
+$(document).ready(function() {
+    t199_showMenu('179624420');
+    t199_positionHeader('179624420');
+});
+$(document).ready(function() {
+    t199_highlight('179624420');
+    t199_checkAnchorLinks('179624420');
+});
+$(document).ready(function() {
+    setTimeout(function() {
+        t_menusub_init('179624420');
+    },500);
+});
+
+//
