7790003a3bba068871c8eb0e7a550fa1fe3e674f
cesnakas <10352420+cesnakas@users.noreply.github.com>
2020-08-31 14:49:57 +0400

    1.3.19

--- local/php_interface/this_site_support.php
+++ local/php_interface/this_site_support.php
@@ -1,1 +1,3 @@
-<a href="https://www.cesnakas.com" target="_blank">www.cesnakas.com</a> | <a href="https://t.me/cesnakas" target="_blank">@cesnakas</a>
+<span class="login-footer-link">
+    <a href="https://www.cesnakas.com" target="_blank">www.cesnakas.com</a> | <a href="https://t.me/cesnakas" target="_blank">@cesnakas</a>
+</span>
